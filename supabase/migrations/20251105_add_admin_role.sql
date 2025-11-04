c320188ebdffddf90d295b816e054e7ae2f7f8e5
Hunter-zip <bartosz2008m@gmail.com>
2025-11-05 00:09:09 +0100

    Update 20251105_add_admin_role.sql

--- supabase/migrations/20251105_add_admin_role.sql
+++ supabase/migrations/20251105_add_admin_role.sql
@@ -1,2 +1,7 @@
-INSERT INTO public.user_roles (user_id, role)
-VALUES ('5c3b5235-2e98-4140-b379-6d54082cd155', 'admin');
+INSERT INTO public.user_roles (id, user_id, role, created_at)
+VALUES (
+  gen_random_uuid(),
+  '5c3b5235-2e98-4140-b379-6d54082cd155',  -- <- wklej swój UUID z tabeli profiles
+  'admin',
+  now()
+);
